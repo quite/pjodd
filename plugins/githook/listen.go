f387ffdc18bd47b50bf5b9532ec8cfd90bda54db
Daniel Lublin <daniel@lublin.se>
2019-07-21 22:44:00 +0200

    Detect type; prepare for gitlab

--- plugins/githook/listen.go
+++ plugins/githook/listen.go
@@ -16,7 +16,7 @@ const (
 )
 
 // Only github for now
-var types = []string{"github"}
+var types = []string{"github", "gitlab"}
 
 type Githook struct {
 	Host   string
@@ -25,7 +25,6 @@ type Githook struct {
 }
 
 type Target struct {
-	Type    string
 	Path    string
 	Secret  string
 	Channel string
@@ -48,10 +47,6 @@ func (gh Githook) Validate(channels []string) error {
 		return fmt.Errorf("no targets configured")
 	}
 	for _, target := range gh.Target {
-		if !contains(types, target.Type) {
-			return fmt.Errorf("target type `%s` is not one of %s",
-				target.Type, types)
-		}
 		if !contains(channels, target.Channel) {
 			return fmt.Errorf("target channel `%s` is not among the configured %s",
 				target.Channel, channels)
@@ -88,21 +83,30 @@ func (gh Githook) doListen(say func(string, string), quit func(string)) {
 		hook, _ := github.New(github.Options.Secret(target.Secret))
 
 		http.HandleFunc(target.Path, func(w http.ResponseWriter, r *http.Request) {
-			payload, err := hook.Parse(r, github.PushEvent, github.PingEvent)
-			if err != nil {
-				log.Printf("hook.Parse: %s\n", err)
-				return
-			}
-			switch payload := payload.(type) {
-			case github.PushPayload:
-				lines := buildPushLines(payload)
-				log.Printf("path:%s channel:%s\n", target.Path, target.Channel)
-				for _, l := range lines {
-					say(target.Channel, l)
-					log.Printf("  %s\n", l)
+			_, fromGithub := r.Header["X-Github-Event"]
+			_, fromGitlab := r.Header["X-Gitlab-Event"]
+			switch {
+			case fromGithub:
+				payload, err := hook.Parse(r, github.PushEvent, github.PingEvent)
+				if err != nil {
+					log.Printf("hook.Parse: %s\n", err)
+					return
 				}
-			case github.PingPayload:
-				log.Printf("pinged: %+v\n", payload)
+				switch payload := payload.(type) {
+				case github.PushPayload:
+					lines := buildPushLines(payload)
+					log.Printf("path:%s channel:%s\n", target.Path, target.Channel)
+					for _, l := range lines {
+						say(target.Channel, l)
+						log.Printf("  %s\n", l)
+					}
+				case github.PingPayload:
+					log.Printf("pinged: %+v\n", payload)
+				}
+			case fromGitlab:
+				log.Printf("gitlab not yet handled")
+			default:
+				log.Printf("webhooked from neither github nor gitlab")
 			}
 		})
 	}
